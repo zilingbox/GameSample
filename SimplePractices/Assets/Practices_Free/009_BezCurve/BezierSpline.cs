ea97655a83254c7ff61e75ccbf2d9a390d219702
ziling <zilingbox@foxmail.com>
2017-05-15 18:33:27 +0800

    add something about the spline maybe there a bug

---
+++ SimplePractices/Assets/Practices_Free/009_BezCurve/BezierSpline.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class BezierSpline : MonoBehaviour {
+
+    public Vector3[] points;
+
+    public int CurveCount
+    {
+        get
+        {
+            return (points.Length - 1) / 3;
+        }
+    }
+
+    public Vector3 GetPoint(float t)
+    {
+        int i;
+        if(t>=1f)
+        {
+            t = 1f;
+            i = points.Length - 4;
+        }else
+        {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= i;
+            i *= 3;
+        }
+        return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], points[3], t));
+    }
+
+    public Vector3 GetVelocity(float t)
+    {
+        int i;
+        if (t >= 1f)
+        {
+            t = 1f;
+            i = points.Length - 4;
+        }
+        else {
+            t = Mathf.Clamp01(t) * CurveCount;
+            i = (int)t;
+            t -= i;
+            i *= 3;
+        }
+        return transform.TransformPoint(Bezier.GetFirstDerivative(points[0], points[1], points[2], points[3], t)) - transform.position;
+    }
+
+    public Vector3 GetDirection(float t)
+    {
+        return GetVelocity(t).normalized;
+    }
+
+    public void Reset()
+    {
+        points = new Vector3[]
+            {
+                new Vector3(1f,0f,0f),
+                new Vector3(2f,0f,0f),
+                new Vector3(3f,0f,0f),
+                new Vector3(4f,0f,0f)
+            };
+    }
+
+    //To add more curve
+    public void AddCurve()
+    {
+        Vector3 point = points[points.Length - 1];
+        //we use Array.Resize method to create larger array to hold the new points,it is inside the system namespace
+        Array.Resize(ref points, points.Length + 3);
+        point.x += 1f;
+        points[points.Length - 3] = point;
+        point.x += 1f;
+        points[points.Length - 2] = point;
+        point.x += 1f;
+        points[points.Length - 1] = point;
+    }
+}
