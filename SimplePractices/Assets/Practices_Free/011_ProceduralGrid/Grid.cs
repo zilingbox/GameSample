0b0abc723704437b4202ec30f9020830fc9f4ee4
ziling <zilingbox@foxmail.com>
2017-05-16 16:59:39 +0800

    add mesh

--- SimplePractices/Assets/Practices_Free/011_ProceduralGrid/Grid.cs
+++ SimplePractices/Assets/Practices_Free/011_ProceduralGrid/Grid.cs
@@ -8,6 +8,7 @@ public class Grid : MonoBehaviour {
 
     public int xSize, ySize;
 
+    private Mesh mesh;
     private Vector3[] vertices;
 
     private void Awake()
@@ -18,11 +19,14 @@ public class Grid : MonoBehaviour {
     private IEnumerator  Generate()
     {
         WaitForSeconds wait = new WaitForSeconds(0.05f);
+
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Procedural Grid";
         //need to hold an array of 3D vectors to store the points
         //the amount of vertices depends on the size of the grid
         //( #x + 1 )( #y + 1 )
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
-        for(int i=0, y=0; y<= ySize; y++)
+        for (int i=0, y=0; y<= ySize; y++)
         {
             for(int x =0; x<= xSize; x++,i++)
             {
@@ -30,6 +34,32 @@ public class Grid : MonoBehaviour {
                 yield return wait;
             }
         }
+
+        mesh.vertices = vertices;
+
+
+        int[] triangles = new int[xSize * ySize * 6];
+        for(int ti =0, vi = 0, y=0; y < ySize; y++,vi++)
+        {
+            for (int x = 0; x < xSize; x++, ti += 6, vi++)
+            {
+                triangles[ti] = vi;
+                triangles[ti + 3] = triangles[ti + 2] = vi + 1;
+                triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
+                triangles[ti + 5] = vi + xSize + 2;
+                mesh.triangles = triangles;
+                yield return wait;
+
+            }
+        }
+        //mesh.triangles = triangles;
+        //triangles[0] = 0;
+        //triangles[3] = triangles[2] = 1;
+        //triangles[4] = triangles[1] = xSize + 1;
+        //triangles[5] = xSize + 2;
+
+
+
     }
 
     private void OnDrawGizmos()
