2f09f31cfb7346c3a0ecae2950eecdc51940b9fc
ziling <zilingbox@foxmail.com>
2017-05-16 18:01:07 +0800

    add normal map

--- SimplePractices/Assets/Practices_Free/011_ProceduralGrid/Grid.cs
+++ SimplePractices/Assets/Practices_Free/011_ProceduralGrid/Grid.cs
@@ -26,16 +26,24 @@ public class Grid : MonoBehaviour {
         //the amount of vertices depends on the size of the grid
         //( #x + 1 )( #y + 1 )
         vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+        Vector2[] uv = new Vector2[vertices.Length];
+        Vector4[] tangents = new Vector4[vertices.Length];
+        Vector4 tangent = new Vector4(1f, 0f, 0f, -1f);
+ 
         for (int i=0, y=0; y<= ySize; y++)
         {
             for(int x =0; x<= xSize; x++,i++)
             {
                 vertices[i] = new Vector3(x, y);
+                uv[i] = new Vector2((float)x / xSize,(float)y / ySize);
+                tangents[i] = tangent;
                 yield return wait;
             }
         }
 
         mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.tangents = tangents;
 
 
         int[] triangles = new int[xSize * ySize * 6];
@@ -48,6 +56,7 @@ public class Grid : MonoBehaviour {
                 triangles[ti + 4] = triangles[ti + 1] = vi + xSize + 1;
                 triangles[ti + 5] = vi + xSize + 2;
                 mesh.triangles = triangles;
+                mesh.RecalculateNormals();
                 yield return wait;
 
             }
